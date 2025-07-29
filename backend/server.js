c9772b63335195ae5a42754fecec967563d56eff
chithraksha16 <chithrakshakharvi@gmail.com>
2025-07-29 13:03:56 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,12 @@ dotenv.config()
 const PORT=process.env.PORT;
 
 
-
+app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cookieParser());
 app.use(cors({
     origin:"https://pingme-theta.vercel.app",
     credentials:true,
-    methods:["GET","POST","DELETE","PUT"]
 }))
 
 
