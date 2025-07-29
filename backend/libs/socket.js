bca69ae0bc15abd4f09f70c80085999c451d7fba
chithraksha16 <chithrakshakharvi@gmail.com>
2025-07-29 19:47:40 +0530

    removed []

--- backend/libs/socket.js
+++ backend/libs/socket.js
@@ -12,7 +12,7 @@ const server=http.createServer(app)
 
 const io= new Server(server,{
     cors:{
-        origin:["https://pingme-theta.vercel.app"],
+        origin:"https://pingme-theta.vercel.app",
         credentials:true
     }
 });
