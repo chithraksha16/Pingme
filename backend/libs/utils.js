b3c244f331e0aeacaa91c39e2f8fe66a8416194c
chithraksha16 <chithrakshakharvi@gmail.com>
2025-07-29 13:17:29 +0530

    secure

--- backend/libs/utils.js
+++ backend/libs/utils.js
@@ -8,7 +8,7 @@ export const generateToken =async(userId,res)=>{
         maxAge:7 *24 *60* 60*1000, // 7 days
         httpOnly: true, //prevent xss attacks
         sameSite: 'None', //prevent csrf attackss
-        secure:process.env.NODE_ENV !== 'development'
+        secure:true
     })
     return token;
 
